0d6c60a743b4093cc7ac256ac8ea533487816cca
EC2 Default User <ec2-user@ip-172-31-0-203.ap-northeast-1.compute.internal>
2022-08-29 09:18:19 +0000

    Chapter 4 投稿機能を作ろう

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'todolists/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get 'top' => 'homes#top'
+  post 'todolists' => 'todolists#create'
 end
